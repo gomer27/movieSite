6b150a98ab93f99116c7398bd6f845c7bb5cdbf2
Ömer Güneş <omergunes2700@gmail.com>
2023-07-07 01:10:00 +0300

    film sitesi için slide ekledim fakat sorun oluştu 1. tıklamada düzgün çalışıyor daha sonra hata veriyor(çok fazla ileri gidiyor)/ I added a slide for the movie site, but there was a problem. It works fine on the 1st click, then it gives an error (goes too far)

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// alert("hello");
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+arrows.forEach((arrow, i) => {
+  let clickCounter = 0;
+  const imageItem = movieLists[i].querySelectorAll("img").length;
+  arrow.addEventListener("click", function () {
+    clickCounter++;
+    if (imageItem - (4 + clickCounter) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 28
+      }rem)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+    }
+  });
+});
